9b194f4ee04f46481af5808238242e7c5af2ba0e
Alexandre Pascoal <apascoal@ubiwhere.com>
2023-08-01 22:14:56 +0100

    feat: Add a test with the plugin

--- cypress/pages/home/home-elements.ts
+++ cypress/pages/home/home-elements.ts
@@ -5,6 +5,7 @@ class Elements {
 	rejectCookiesBtn = () => cy.get('#W0wltc');
 
 	title = () => cy.get('img[alt="Google"]');
+	titleAnimated = () => cy.get('#hplogo');
 	searchInput = () =>  cy.get('#APjFqb');
 	searchBtn = () => cy.get('form[action="/search"] > div:nth-child(1) > div:nth-child(1) > ' +
 		'div:nth-child(6) > center > input[name="btnK"]');
